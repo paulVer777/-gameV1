72add78614d55fca043d1bab41be99e7f06fe22d
p <pawel.gitara@onet.eu>
2019-02-19 00:28:54 +0100

    gui in progress

--- index.js
+++ index.js
@@ -2,75 +2,116 @@
 class Game {
 
     constructor(){
-      this.fields=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+      this.fields=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
       this.diceThrow= null
-      this.currentPosition= 0   
+      this.currentPosition= 1   
       this.throws=0   
       this.sum=0
     }
-    
+    messenger(data){
+      return ({
+          welcome: 'Hello ! :) To play just press space on you keyboard.',
+          throw :`Wyrzuciłeś ${this.diceThrow}. Press space to throw a dice.`,
+          field19: `Wyrzucono ${this.diceThrow}. Ups...pole numer 19. Wracasz do pola 11.`,
+          tooFar: `Wyrzucono: ${this.diceThrow}. Przekroczyłeś pole 20 o ${data}. Wracasz na pole ${this.currentPosition}.`,
+          gameOver: 'Ups pole 12. Game over. Press space to play again.',
+          victory: `Victory. Próby: ${this.throws}. Średnia: ${this.sum/this.throws}. Press space to play again.`
+      })
+    }
+
+    makeBoard(){
+
+      document.querySelector('.board').innerHTML = ""
+
+      this.fields.map((value,index) => {
+
+        const div = document.createElement('div')
+        
+        div.classList.add('field') 
+        value === 12 || value ===19 ? div.classList.add('bad-field') : null 
+        value===20&& div.classList.add('win-field')
+        value===this.currentPosition&&div.classList.add('current-position')
+        value !== 21 ? div.innerHTML=value : div.innerHTML='...'
+
+        document.querySelector('.board').appendChild(div)
+
+      })
+    }
+
+    showMessage(message){
+      document.querySelector('.commentator').innerHTML=message
+    }
+
     diceThrower(){
 
         const num = Math.floor(Math.random()*6+1)
         this.diceThrow = num 
         this.throws++
         this.sum +=num
-        console.log(`Wyrzuciłeś ${this.diceThrow}`)
-    }
-    whereAmI(){
-      this.currentPosition+=this.diceThrow
-      console.log(`Znajdujesz się na polu o numerze ${this.currentPosition}`)
+        this.showMessage(this.messenger().throw)
     }
 
-     calcAverage() {
-       return Math.round((this.sum / this.throws) * 100 / 100)
-     }
-     resetGame() {
+    whereAmI(){
+      this.currentPosition+=this.diceThrow
+      setTimeout(() => this.makeBoard(), 500)
+    }
+
+    resetGame() {
        this.diceThrow = null
-       this.currentPosition = 0
+       this.currentPosition = 1
        this.throws = 0
        this.sum = 0
      }
 
     gameGuard(){
-
       const position=this.currentPosition
 
       switch(true){
         case ( position === 20 ): {
-          console.log(`You win! Throws:${this.throws} Scores:${this.calcAverage()}. Press any key to play again.`)
+
+          // document.querySelector('.board').childNodes[20].classList.add('happy')
+
+          this.showMessage(this.messenger().victory)
           this.resetGame()
+          setTimeout(()=>this.makeBoard(),2000)
         }
           break;
         case (position === 19): {
           this.currentPosition=11
-          console.log(`Ups, wracasz na pole o numerze 11`)
+          this.showMessage(this.messenger().field19)
+          setTimeout(()=>this.makeBoard(),2000)       
         }
           break;
         case (position === 12): {
-          console.log('Game Over. Press any key to play again.')
+          this.showMessage(this.messenger().gameOver)
           this.resetGame()
+          setTimeout(() => this.makeBoard(), 2000)
         }
           break;
         case (position > 20): {
           const num = this.currentPosition - 20
           this.currentPosition = 20 - num
-          console.log(`Przekroczyłeś 20 wracasz na pole ${this.currentPosition}`)
+          this.showMessage(this.messenger(num).tooFar)
+          setTimeout(()=>this.gameGuard(),2000)
+          setTimeout(() => this.makeBoard(), 2000)
         }
           break;
         default: {
-          console.log('Throw a dice!')
+          // this.showMessage(this.messenger().throw)
         }
       }
     }
-   
 }
 
 const game = new Game()
-
-window.addEventListener("keypress",(event)=> {
-
-  game.diceThrower()
-  game.whereAmI()
-  game.gameGuard()
+ 
+  game.makeBoard()
+  game.showMessage(game.messenger().welcome)
+  
+  window.addEventListener("keypress",(event)=> {
+     if(event.key === ' '){
+      game.diceThrower()
+      game.whereAmI()
+      game.gameGuard()
+    }
 })
